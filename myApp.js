cb3f5c8a229f41a3081c6e8532ee93f09ae5798d
Bundtologist <kilelol@hotmail.com>
2024-08-02 21:32:01 -0400

    v1

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+
+
+const mongoose = require('mongoose');
 
 let Person;
 
